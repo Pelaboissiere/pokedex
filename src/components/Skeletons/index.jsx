7acd891e2751ccc6c976a622d26379785c12538e
Pedro <pelaboissiere@gmail.com>
2024-05-23 11:12:44 -0300

    First Commit

---
+++ src/components/Skeletons/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Skeleton } from '@mui/material'
+
+export const Skeletons = () => {
+    return (
+        <Container maxWidth="xxl">
+            <Skeleton variant="rounded" width="100%" height={150} sx={{ marginBottom: "1em" }}/>
+            <Skeleton variant="rounded" width="100%" height={150} sx={{ marginBottom: "1em" }}/>
+            <Skeleton variant="rounded" width="100%" height={150} sx={{ marginBottom: "1em" }}/>
+            <Skeleton variant="rounded" width="100%" height={150} sx={{ marginBottom: "1em" }}/>
+            <Skeleton variant="rounded" width="100%" height={150} sx={{ marginBottom: "1em" }}/>
+        </Container>
+    )
+}
+ 
